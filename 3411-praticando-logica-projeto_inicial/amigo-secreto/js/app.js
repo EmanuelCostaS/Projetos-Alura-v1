f9982c4e463b0043552fcae3f7598f9752c6455c
Emanuel Silva <emanuel_silva2004@hotmail.com>
2024-01-06 20:55:43 -0300

    Projeto finalizado

--- 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,35 +1,60 @@
-let nomes = ['a', 'b', 'c', 'd'];
+
+let nomes = [];
+let listaSorteio = document.getElementById('lista-sorteio');
+let nome;
 
 function adicionar(){
-    let nome = document.getElementById('nome-amigo').value;
+    nome = document.getElementById('nome-amigo').value;
     nomes.push(nome);
-    console.log(`O nome secreto é ${nome}, os nomes inseridos foram ${nomes}`);
+    console.log(`O nome é ${nome}, os nomes inseridos foram ${nomes}`);
 
     if (nomes.length == 1){
         document.getElementById('lista-amigos').append(nome);
-    } else {document.getElementById('lista-amigos').append(', '+ nome);}
+    } else {
+        document.getElementById('lista-amigos').append(', '+ nome);
+    }
 }
 
 function sortear(){
+    nome = document.getElementById('nome-amigo').value;
+    listaSorteio = document.getElementById('lista-sorteio');
     let jaSorteados = [];
-    let listaAssociados = [];
-    let i = 0;
+    let i = jaSorteados.length;
     let max = nomes.length;
-    let numeroSorteado;
 
-    while (jaSorteados.length < max){
-        numeroSorteado = parseInt(Math.random() * (max));
-        if (numeroSorteado != i && typeof jaSorteados[numeroSorteado] === 'undefined'){
-            jaSorteados.push(numeroSorteado);
-        } else {}
-        console.log(numeroSorteado);
-        console.log(jaSorteados);
-        
+    while(jaSorteados.length < max){
+        numeroSorteado = parseInt(Math.random() * (max + 1));
+        if (numeroSorteado !== i && !jaSorteados.includes(numeroSorteado - 1)){
+            jaSorteados.push(numeroSorteado - 1);
+        }
     }
 
+    let nomesSorteados = [];
+    for (let j = 0; j < jaSorteados.length; j++){
+        console.log(jaSorteados[j])
+        for (k = 0; k < nomes.length; k++) {
+            if (jaSorteados[j] === k){
+                nomesSorteados.push(nomes[jaSorteados[j]]);
+            }
+            //console.log(nomes[k]);
+          } 
+          console.log(nomesSorteados);     
+    }
+    //console.log(jaSorteados);
     
+    for (let m = 0; m < nomes.length; m++){
+        listaSorteio.innerHTML = listaSorteio.innerHTML +  nomes[m] + ' --> ' + nomesSorteados[m] + '<br>'
+    }    
 }
 
 function reiniciar(){
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('nome-amigo').innerHTML = '';
 
+    nomes = [];
+    jaSorteados = [];
+    nomesSorteados = [];
+    nome = '';
+    listaSorteio.innerHTML = '';
+    
 }
